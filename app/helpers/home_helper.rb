d5581274caccd5e9bcb47487a3763b16bdc7c0de
Omar Ragheb <92755394+omar25ahmed@users.noreply.github.com>
2023-04-11 05:39:46 +0200

    personalize home page (#10)

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,6 @@
 module HomeHelper
+  def generate_color(string)
+    srand(string.hash) # seed the random number generator with the hash of the argument
+    '#' + ("%06x" % rand(0..0xffffff)) # generate a random 6-digit hex code
+  end
 end
