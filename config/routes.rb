8051282f5472e9ffdf0daba5f5451c745bdf652a
Omar Ragheb <ahmed.raph29@gmail.com>
2023-04-17 17:31:01 +0200

    Product page

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get 'contact', to: 'home#contact'
   post 'contact_index', to: 'home#contact'
   get 'about', to: 'home#about'
+
+  # products
+  get 'products', to: 'products#index'
 end
