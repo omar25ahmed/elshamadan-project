0e67b0da3fcf9c2c2556beb1bf679f96709a807b
Omar Ragheb <ahmed.raph29@gmail.com>
2023-04-19 05:03:43 +0200

    wrap up task

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,5 @@
 class Brand < ApplicationRecord
+  validates :name, :description, :name_ar, :description_ar, presence: true
   has_many :products
   has_one_attached :logo
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'about', to: 'home#about'
 
   # products
-  get 'products', to: 'products#index'
+  resources :products, only: [:index]
 
   # brands
   resources :brands, only: [:show]
