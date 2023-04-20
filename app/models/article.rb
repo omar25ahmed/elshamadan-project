56086ff1a0d64c8dbdda12122536b21205404338
Omar Ragheb <ahmed.raph29@gmail.com>
2023-04-20 21:21:28 +0200

    Add article validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   has_one_attached :cover
+  validates :title, :body, presence: true
+  validates :cover, attached: true
+  validates :title, length: { minimum: 5 }
+  validates :body, length: { minimum: 100 }
 end
