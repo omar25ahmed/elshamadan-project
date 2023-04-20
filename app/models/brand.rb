5a019246f8d438750b31103cc0bb99ddc7bd98a6
Omar Ragheb <92755394+omar25ahmed@users.noreply.github.com>
2023-04-20 03:45:07 +0200

    Add validation for attachments (#13)

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,5 +1,6 @@
 class Brand < ApplicationRecord
   validates :name, :description, :name_ar, :description_ar, presence: true
+  validates :logo, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
   has_many :products
   has_one_attached :logo
 end
