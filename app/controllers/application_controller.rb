d2095fc6ef03393808fde17e922e0e89dd152ecd
Omar Ragheb <ahmed.raph29@gmail.com>
2023-04-23 11:10:38 +0200

    localize home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
     default_url_options[:locale] = I18n.locale
   end
+
+  def default_url_options(options = {})
+    (I18n.locale.to_sym.eql?(I18n.default_locale.to_sym) ? {} : {locale: I18n.locale}).merge options
+  end
 end
