507fd9dd2ee3e3afc82527c42c0a0e16d1c0e1ff
Omar Ragheb <ahmed.raph29@gmail.com>
2023-04-23 09:29:35 +0200

    Add the setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#index'
 
-  # contact for index post and get
-  get 'contact', to: 'home#contact'
-  post 'contact_index', to: 'home#contact'
-  get 'about', to: 'home#about'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+  # your routes go here
+    root 'home#index'
+    # contact for index post and get
+    get 'contact', to: 'home#contact'
+    post 'contact_index', to: 'home#contact'
+    get 'about', to: 'home#about'
+    # products
+    resources :products, only: [:index]
+    # brands
+    resources :brands, only: [:show]
+    # articles
+    resources :articles, only: [:index, :show]
+  end
 
-  # products
-  resources :products, only: [:index]
-
-  # brands
-  resources :brands, only: [:show]
-
-  # articles
-  resources :articles, only: [:index, :show]
 end
